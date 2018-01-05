67f9803908519f8e96879106aeaabe37a0b932d8
Mustafa Paksoy <paksoy@gmail.com>
2018-01-05 15:40:29 -0800

    Working spring boot version

---
+++ src/main/java/net/paksoy/app/IndexController.java
@@ -0,0 +1,12 @@
+package net.paksoy.app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello 2";
+    }
+}
